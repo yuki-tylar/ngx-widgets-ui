1b2f8433d7ed75bca97d51986dc00b8fb19d6175
takayuki_hiraishi <takayuki.hiraishi@gmail.com>
2021-02-07 12:38:47 -0800

    small changes

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -8,6 +8,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
+  public darkmode = 'disable';
+  public color = 'default';
+  public colors = ['default', 'blue', 'green', 'orange', 'yellow', 'grey-dark']
+
+  get darkClass(){
+    let c = '';
+    switch(this.darkmode){
+      case 'disable': c = ''; break;
+      case 'auto': c = 'dark-auto'; break;
+      case 'enable': c = 'dark'; break;
+    }
+    return c;
+  }
+
   onClickButton(e:any){
+  }
+
+  changeDarkMode(state: string){
+    this.darkmode = state;
+  }
+
+  changeColor(color: string){
+    this.color = color; 
   }
 }
